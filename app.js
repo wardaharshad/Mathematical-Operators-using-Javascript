a89cf992b26250db92f9846a00a5812ff068db59
Wardah Arshad <54935867+wardaharshad@users.noreply.github.com>
2022-09-29 03:13:33 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,255 @@
+// alert("Hello World")
+
+
+// var name = "Hello World" ;
+
+// console.log(name)
+
+
+
+// var num1 = 30 ;
+
+// var num2 = 10 ;
+
+// console.log( num1 + num2 )
+// console.log( num1 - num2 )
+// console.log( num1 * num2 )
+// console.log( num1 / num2 )
+
+
+// console.log( num1 % num2  )
+
+
+// var num1 = "10" ;
+
+// var num2 = 10 ;
+
+// console.log( num1 + num2  )
+
+// var num3 = "30" ;
+
+// var num4 = "40" ;
+
+// console.log(num3 + num4)
+
+// var num5 = 50 + 60 + 40 + num3 + num4 ;
+
+// console.log(num5); 
+
+// console.log("HElo World");
+
+
+
+
+// var a = 10 ;
+
+// console.log( ++a )
+
+// var b = 10 
+
+// console.log( b++ )
+
+// console.log(b)
+
+// var num = 5;
+// num++ ;
+// console.log(num)
+
+
+
+// var a = 7 ;
+
+// console.log(--a)
+
+// var b = 8 ; 
+
+// --b ;
+
+// console.log(b)
+
+
+// var a = 5 ;
+
+//               5 + 6 + 6 + 7
+// var calculation = a + ++a + a++ + a ;
+
+//           5 +  4  + 4  + 4 + 4 + 4 + 4
+// var result = a + --a + a-- + ++a + a + a + a ;
+
+
+// console.log(calculation)
+
+// console.log(result)
+
+
+// console.log( 60 == 50 )
+
+// console.log( 60 === 60 )
+
+
+// var a = 10 ;
+
+// var b = "10" ;
+
+// console.log( a === b )
+// console.log( a == b )
+// console.log( num1 > num2 )
+// console.log( num1 < num2 )
+
+// var num1 = 50; 
+// var num2 = 50;
+
+
+// console.log( num1 >= num2 )
+
+// console.log( num1 <= num2  )
+
+
+// var a = 10 ;
+
+// var b = "10" ;
+
+// console.log( a != b  ) ;
+
+// console.log( a !== b ) ;
+
+
+// var input = prompt("Enter Your Name" , "Enter Name")
+
+// console.log(input)
+
+
+// if(condition){
+//     block of statement
+// }
+// else{
+
+// }s
+
+// var num = 20 ;
+
+// if( num === 30){
+//     console.log("I am IF")
+// }
+// else if(num === 20){
+//     console.log("I am Else If")
+// }
+// else{
+//     console.log("I am Else")
+// }
+
+// var day = prompt("Enter Day");
+
+// if(day == "Sunday"){
+
+//     alert("Its a Fun Day")
+
+// }
+// else if( day == "Saturday"){
+
+//     alert("Its a Fun day")
+
+// }
+// else{
+//     alert("its not a Fun day")
+// }
+
+// var data = +prompt("Enter Name")
+
+// console.log( typeof(data)  )
+
+
+// Calculator 
+
+
+var num1 = +prompt("Enter First Number")
+var opt = prompt("Enter operator")
+var num2 = +prompt("Enter Second Number")
+
+
+if( opt == "+" ){
+    console.log(num1 + num2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
